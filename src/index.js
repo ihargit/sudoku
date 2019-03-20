911864c7e11ab2aa6734967fe07d4ebaa1e526e4
ihargit <ihar.kharashun@gmail.com>
2019-03-20 15:54:35 +0300

    feat: add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  let zeroPosition = returnZeroPosition(matrix);
+
+  if (zeroPosition[0] === 1) {
+
+  }
+
+
+
+
+
+
+  
+}
+
+function returnZeroPosition (matrix) {
+  let position = [-1, -1]; // [row, column]
+  for (let i = 0; i < matrix.length; i++) {
+    position[1] = matrix[i].indexOf(0);
+    if (position[1] != -1) {
+      position[0] = i;
+      break;
+    }
+  }
+  return position;
 }
